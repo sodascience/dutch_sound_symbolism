598fcf86c39fd6722dc3f666380b20c7abf17058
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2023-01-26 15:37:44 +0100

    Remove superfluous print statement

--- 01_preprocessing.R
+++ 01_preprocessing.R
@@ -6,7 +6,7 @@ vankrunkelsven_2022 <- read_delim(
   file = "raw_data/vankrunkelsven_2022/Norms_Gender.csv", 
   delim = ";", col_types = vankrunkelsven_2022_spec
 )
-vankrunkelsven_2022
+
 write_rds(vankrunkelsven_2022, "processed_data/vankrunkelsven_2022.rds")
 
 # brysbaert 2014 ----
